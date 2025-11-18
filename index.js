97d3f9b478adf73c5c1998c8430bb28c9763ba86
Fathimahmoud10 <Fathimahmoudf0@gmail.com>
2025-11-19 00:21:55 +0200

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 
 async function startBrowser() {
     const browser = await puppeteer.launch({
-        headless: true,
+        headless: "new",
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
@@ -13,14 +13,10 @@ async function startBrowser() {
         ]
     });
 
-    console.log("Browser started successfully on Render");
-
-    // لو محتاج ترجع browser لكود تاني
+    console.log("Browser started on Render");
     return browser;
 }
 
-// شغّل المتصفح
 startBrowser().catch(console.error);
 
-// شغّل السيرفر
 require("./server.js");
