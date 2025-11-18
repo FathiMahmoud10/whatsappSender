e8da240ac1397afa2aa1ebf95782851ff9c6a731
Fathimahmoud10 <Fathimahmoudf0@gmail.com>
2025-11-19 00:18:37 +0200

    update

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const puppeteer = require("puppeteer");
+
+async function startBrowser() {
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu',
+            '--no-zygote',
+            '--single-process'
+        ]
+    });
+
+    console.log("Browser started successfully on Render");
+
+    // لو محتاج ترجع browser لكود تاني
+    return browser;
+}
+
+// شغّل المتصفح
+startBrowser().catch(console.error);
+
+// شغّل السيرفر
 require("./server.js");
